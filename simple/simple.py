7024960ab685dd5eaa0000dd84e0ba42e893a2e8
csluduena <csluduena@gmail.com>
2025-06-25 13:47:33 -0300

    implementar simple_list: generar 10 diccionarios con id y age aleatorio

--- simple/simple.py
+++ simple/simple.py
@@ -2,7 +2,21 @@ import random
 
 
 def simple_list():
-    pass
+    """
+    Genera una lista de 10 diccionarios con id único y age aleatorio.
+
+    Returns:
+        Lista de 10 diccionarios con claves 'id' y 'age'
+    """
+    persons = []
+    for i in range(10):
+        person = {
+            "id": i + 1,  # ID único empezando desde 1
+            "age": random.randint(1, 100)  # Edad aleatoria entre 1 y 100
+        }
+        persons.append(person)
+
+    return persons
 
 
 def sort_list():
